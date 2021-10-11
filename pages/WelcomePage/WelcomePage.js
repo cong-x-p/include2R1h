2a0d85f3a13aa581ac9904fdaa9b7f6cba3d39fe
congxiping <congxiping@pyou.com>
2021-10-11 18:43:42 +0800

    damn

--- pages/WelcomePage/WelcomePage.js
+++ pages/WelcomePage/WelcomePage.js
@@ -12,7 +12,7 @@ class WelcomePage extends React.Component {
                         <Image
                             priority
                             className={styles.circleImg}
-                            src="/images/me.PNG"
+                            src="/me.PNG"
                             alt="me"
                             height={200}
                             width={200}
