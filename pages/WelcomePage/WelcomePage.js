a35f196fea69975c87e5fe89d5bd2ef459f8ae73
congxiping <congxiping@pyou.com>
2021-10-11 18:13:41 +0800

    update with welcome page

---
+++ pages/WelcomePage/WelcomePage.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Image from "next/image";
+import Container from "@mui/material/Container";
+import styles from "./WelcomPage.module.css";
+
+class WelcomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <Container maxWidth={"md"}>
+                    <div className={styles.container}>
+                        <Image
+                            priority
+                            className={styles.circleImg}
+                            src="/images/me.png"
+                            alt="me"
+                            height={200}
+                            width={200}
+                        />
+                    </div>
+                    <div className={styles.Welcome}>
+                        Welcome to my site
+                    </div>
+                    <div className={styles.littletips}>
+                        <p>(╯▔▽▔)╯ plz don't go away that quickly<br/>read the passage below, maybe you'll be insterested</p>
+                    </div>
+                    <div className={styles.self_descripton}>
+                        I'm Sebastian Cong by the way
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default WelcomePage;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import SignIn from "./SignIn/SignIn";
+import Head from "next/head";
+import WelcomePage from "./WelcomePage/WelcomePage";
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <SignIn/>
+                <Head>
+                    <title>2R1h's Page</title>
+                </Head>
+                <WelcomePage/>
             </div>
         );
     }
